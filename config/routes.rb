6e73e109d403eedb7a9ea3aabf967df7068b2387
Karl Heitmann <khf1988@gmail.com>
2021-03-12 17:13:42 -0300

    anidado books dentro de categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :books
-  root to: 'books#index'
+  resources :categories do
+    resources :books
+  end
+  root to: 'categories#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
